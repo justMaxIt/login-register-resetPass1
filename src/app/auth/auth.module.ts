17df05364164e4c085e4a968415b40870e9a16c6
Maksym Fedorenko <justmaksit@gmail.com>
2021-06-25 16:07:07 +0300

    add ngForm and onSubmit func, && styled inputs

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule
   ],
   exports: [
     LoginComponent,
